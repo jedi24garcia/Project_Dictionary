b78bb740d277215925645737df86d8af23c823b5
jedi24garcia <jedi24garcia@gmail.com>
2022-09-12 12:50:53 +1200

    Convert to Python3

--- main2.py
+++ main2.py
@@ -7,11 +7,12 @@ from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 root = tk.Tk()
 
-root.geometry('570x500')
-root.title('Waterford')
-root.attributes('-alpha', 0.9)
-root.attributes('-topmost', 1)
-#root.iconbitmap = need to save an ".ico" file. 
+class CoolThing:
+  def __init__(self, root):
+    self.root.geometry('570x500')
+    self.root.title('Waterford')
+    self.root.attributes('-topmost', 1)
+# root.iconbitmap = need to save an ".ico" file. 
 
 def dict(event=None):
   meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
@@ -20,20 +21,35 @@ Label(root, text="Private Dictionary", font=(
   "Poppins, 20 bold"), fg="Orange").pack(pady=20)
 
 frame = Frame(root)
-Label(frame, text="Enter random: ", font=("Helvetica, 15 bold")).pack(side="left")
-word = Entry(frame, font=("Helvetica, 15 bold"), width=30)
+Label(frame, text="Enter word: ", font=("Helvetica, 15 bold")).pack(side="left")
+word = Entry(frame, font=("Helvetica, 15 bold"), width=10)
 word.pack()
 frame.pack(pady=10)
 
+"""
 frame1 = Frame(root)
 Label(frame1, text="Result: ", font=("Helvetica, 10 bold")).pack(side="left")
-meaning = Label(frame1, text="", font=("Helvetica 10"))
+#meaning = Label(frame1, text="", font=("Helvetica 10"))
+meaning = Label(frame1, text="", font=("Helvetica 10"), height=20, width=65)
+meaning.pack(pady=10)
+frame1.pack(pady=10)
+"""
+
+frame1 = Frame(root)
+Label(frame1, text="Results: ", font="Helvetica, 10 bold")
+meaning = Text(root, height=20, width=65) 
 meaning.pack()
 frame1.pack(pady=10)
+
+# just a sample
+"""
+my_text = Text(root, height=20, width=65, wrap=WORD)
+my_text.pack(pady=10)
+"""
 
 word.bind('<Return>', dict)
 
 root.mainloop() # runs the GUI or program
 
 
-#use this website for reference: https://www.geeksforgeeks.org/word-dictionary-using-tkinter/
+# use this website for reference: https://www.geeksforgeeks.org/word-dictionary-using-tkinter/
