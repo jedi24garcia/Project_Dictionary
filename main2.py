cc2b1e966f9d4394fa09541575a65c6b2a0cfb5b
jedi24garcia <jedi24garcia@gmail.com>
2022-10-26 21:26:33 +1300

    modified variable

--- main2.py
+++ main2.py
@@ -6,30 +6,30 @@ from PyDictionary import PyDictionary
 import json
 
 dictionary = PyDictionary()
-root = tk.Tk()
+window  = tk.Tk()
 
-root.title("My Personal Assistance")
-root.geometry("900x600+100+100")
-root.attributes("-topmost", 1)
-root.configure(bg="black")
+window.title("My Personal Assistance")
+window.geometry("900x600+100+100")
+window.attributes("-topmost", 1)
+window.configure(bg="black")
 
 
 def dict(event=None):
   meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
 
-Label(root, text="Private Dictionary", font=(
+Label(window, text="Private Dictionary", font=(
   "Poppins, 20 bold"), fg="Orange").pack(pady=20)
 
-frame = Frame(root)
+frame = Frame(window)
 Label(frame, text="Enter word: ", font=("Helvetica, 15 bold")).pack(side="left")
 word = Entry(frame, font=("Helvetica, 15 bold"), width=10)
 word.pack()
 frame.pack(pady=10)
 
-meaning = Label(root, height=25, width=95) 
+meaning = Label(window, height=25, width=95) 
 meaning.pack()
 
 word.bind('<Return>', dict)
 
-root.mainloop()
+window.mainloop()
  
