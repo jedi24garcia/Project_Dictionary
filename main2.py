f3ead27cdab26d70d0846834ad75ae92a675178e
jedi24garcia <jedi24garcia@gmail.com>
2022-12-07 22:16:57 +1300

    back to normal

--- main2.py
+++ main2.py
@@ -3,19 +3,7 @@
 import tkinter as tk
 from tkinter import *
 from PyDictionary import PyDictionary
-import speech_recognition as sr
 import pyttsx3
-# import time
-# import datetime
-# from ecapture import ecapture as ec
-# import requests
-# import subprocess
-# import webbrowser
-# import wikipedia
-# import wolframalpha
-# import os
-
-# print("Welcome!")
 
 dictionary = PyDictionary()
 window = tk.Tk()
@@ -25,23 +13,10 @@ engine = pyttsx3.init()
 window.title("Personal Dictionary")
 window.geometry("900x600+100+100")
 window.attributes("-topmost", 1)
-window.configure(bg="darkgrey")
+window.configure(bg="black")
 
 def dict(event=None):
   meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
-
-# will need to figure this out
-
-"""
-# audio
-def takeCommand():
-  r=sr.Recognizer()
-  with sr.Microphone() as source:
-  # print("Listening...")
-    audio=r.Listen(source)
-"""
-
-# speak("Welcome!")
 
 Label(window, text="System Dictionary", font=("Helvitica, 40 bold"), fg="Black").pack(pady=20)
 
@@ -57,11 +32,4 @@ meaning.pack()
 
 word.bind('<Return>', dict)
 
-def speak(text):
-  engine.say(text)
-  engine.runAndWait()
-
-speak("Welcome")
-
-window.mainloop()
- 
+window.mainloop() 
