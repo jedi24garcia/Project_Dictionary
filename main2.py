f32a47e17e23ea0503cfc12fd4d15622838e1716
jedi24garcia <jedi24garcia@gmail.com>
2023-01-09 18:31:14 +1300

    added new window button however error when clicking on it

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 from tkinter import *
-from tkinter.ttk import *
+# from tkinter.ttk import *
 from PyDictionary import PyDictionary
 
 dictionary = PyDictionary()
@@ -17,18 +17,20 @@ window.configure(bg="black")
 def dict(event=None):
   meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
 
-"""
-class NewWindow():
-  def __init__(self, new_window):
-    self.new_window=new.window
+class NewWindow(Toplevel):
+  def __init__(self, window = None):
+    super().__init__(window = window)
     self.title("New window")
     self.geometry("900x600+100+100")
-    self.
     label = Label(self, text = "This is a new window")
     label.pack()
-"""
 
 Label(window, text="System Dictionary", font=("Helvitica, 40 bold"), fg="Red").pack(pady=20)
+
+'''
+label = Label(window, text = "System Dictionary")
+label.pack(side = TOP, pady = 10)
+'''
 
 frame = Frame(window)
 Label(frame, text="Enter word: ", font=("Helvetica, 15 bold")).pack(side="left")
@@ -39,9 +41,10 @@ frame.pack(pady=10)
 meaning = Label(window, height=25, width=95) 
 meaning.configure(bg="black")
 meaning.pack()
-# btn = Button(window, text="Click to open new window", command = NewWindow)
-# btn.pack(pady = 10)
+btn = Button(window, text="Click to open new window")
+btn.bind("<Button>", lambda e: NewWindow(window))
+btn.pack(pady = 10)
 
 word.bind('<Return>', dict)
 
-window.mainloop() 
+window.mainloop()
